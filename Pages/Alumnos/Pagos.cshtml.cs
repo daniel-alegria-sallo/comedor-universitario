1cc889e22268dc35ff55dc38a5d89cadbd57ba4b
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2024-07-20 11:47:45 -0500

    Añade pagina de pagos.

---
+++ Pages/Alumnos/Pagos.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace ComedorUniversitario.Pages.Alumnos;
+
+public class PagosModel : PageModel
+{
+    private readonly ILogger<PagosModel> _logger;
+    public String errorMessage = "";
+    public String successMessage= "";
+    public Pago pago = new Pago();
+
+    public PagosModel(ILogger<PagosModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        pago.alumnoId = Request.Form["alumno"];
+        pago.codMatricula = Request.Form["codMatricula"];
+
+        if (
+                pago.alumnoId.Length == 0
+                || pago.codMatricula.Length == 0
+           )
+        {
+            errorMessage = "Faltan ingresar datos";
+            return;
+        }
+
+        //
+        try
+        {
+            String connectionString = "Server=(local);Database=testdb;User Id=sa;Password=Ce/danielonsql;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                String sql = "exec proc_pagar @codAlumno, @codMatricula";
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.Parameters.AddWithValue("@codAlumno", pago.alumnoId);
+                    command.Parameters.AddWithValue("@codMatricula", pago.codMatricula);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            errorMessage = ex.Message;
+            return;
+        }
+
+        //
+        pago.clear();
+        successMessage = "Proceso Exitoso";
+
+        // Response.Redirect("/Alumnos/Reportes");
+    }
+}
+
+public class Pago
+{
+    public String alumnoId;
+    public String codMatricula;
+    public void clear()
+    {
+        alumnoId = "";
+        codMatricula = "";
+    }
+}
